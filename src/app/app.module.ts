ae656424caabb5c0de714a5209b7399208ea3e8b
Aravindkumar-Murugesan <aravindgithub.uuull@dralias.com>
2023-05-31 22:50:38 +0530

    Creating custom pipes and using angular pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import {employeeComponent} from '../employee/employee.component';
 import {employeeListComponent} from '../employee/employeeList.component';
+import { employeeTitle } from 'src/employee/employeeTitle.pipe';
 @NgModule({
   declarations: [
-    AppComponent,employeeComponent,employeeListComponent
+    AppComponent,employeeComponent,employeeListComponent,employeeTitle
   ],
   imports: [
     BrowserModule,
--- src/employee/employeeList.component.ts
+++ src/employee/employeeList.component.ts
@@ -13,7 +13,7 @@ export class employeeListComponent {
         this.employees = [
             {
                 code: 'emp101', name: 'Tom', gender: 'Male',
-                annualSalary: 5500, dateOfBirth: '25/6/1988'
+                annualSalary: 5500, dateOfBirth: '2/6/1988'
             },
             {
                 code: 'emp102', name: 'Alex', gender: 'Male',
@@ -25,7 +25,7 @@ export class employeeListComponent {
             },
             {
                 code: 'emp104', name: 'Mary', gender: 'Female',
-                annualSalary: 6500.826, dateOfBirth: '14/10/1980'
+                annualSalary: 6500.826, dateOfBirth: '1/10/1980'
             },
         ];
     }
@@ -34,7 +34,7 @@ export class employeeListComponent {
         this.employees = [
             {
                 code: 'emp101', name: 'Tom', gender: 'Male',
-                annualSalary: 5500, dateOfBirth: '25/6/1988'
+                annualSalary: 5500, dateOfBirth: '2/6/1988'
             },
             {
                 code: 'emp102', name: 'Alex', gender: 'Male',
@@ -46,11 +46,11 @@ export class employeeListComponent {
             },
             {
                 code: 'emp104', name: 'Mary', gender: 'Female',
-                annualSalary: 6500.826, dateOfBirth: '14/10/1980'
+                annualSalary: 6500.826, dateOfBirth: '1/10/1980'
             },
             {
                 code: 'emp105', name: 'Nancy', gender: 'Female',
-                annualSalary: 6700.826, dateOfBirth: '15/12/1982'
+                annualSalary: 6700.826, dateOfBirth: '1/12/1982'
             },
         ];
     }
