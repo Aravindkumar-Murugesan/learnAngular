99768f82655513781b9549be78614366d7097fe5
Aravindkumar Murugesan <Aravindkumar.Murugesan@Tracque.com>
2023-06-07 09:08:05 +0530

    Interface

--- src/employee/employeeList.component.ts
+++ src/employee/employeeList.component.ts
@@ -1,56 +1,74 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
+import { IEmployee } from './emploee';
 
 @Component({
-    selector:'employee-list',
-    templateUrl : '/employeeList.component.html',
-    styleUrls: ['/employeeList.component.css']
+  selector: 'employee-list',
+  templateUrl: '/employeeList.component.html',
+  styleUrls: ['/employeeList.component.css'],
 })
-
 export class employeeListComponent {
-    employees: any[];
-    selectedEmployeeCount : string = 'All';
-    constructor() {
-        this.employees = [
-            {
-                code: 'emp101', name: 'Tom', gender: 'Male',
-                annualSalary: 5500, dateOfBirth: '2/6/1988'
-            },
-            {
-                code: 'emp102', name: 'Alex', gender: 'Male',
-                annualSalary: 5700.95, dateOfBirth: '9/6/1982'
-            },
-            {
-                code: 'emp103', name: 'Mike', gender: 'Male',
-                annualSalary: 5900, dateOfBirth: '12/8/1979'
-            },
-            {
-                code: 'emp104', name: 'Mary', gender: 'Female',
-                annualSalary: 6500.826, dateOfBirth: '1/10/1980'
-            },
-            {
-                code: 'emp105', name: 'Nancy', gender: 'Female',
-                annualSalary: 6700.826, dateOfBirth: '1/12/1982'
-            },
-            {
-                code: 'emp106', name: 'Steve', gender: 'Male',
-                annualSalary: 7700.481, dateOfBirth: '11/18/1979'
-            }
-        ];
-    }
+  employees: IEmployee[];
+  selectedEmployeeCount: string = 'All';
+  constructor() {
+    this.employees = [
+      {
+        code: 'emp101',
+        name: 'Tom',
+        gender: 'Male',
+        annualSalary: 5500,
+        dateOfBirth: '2/6/1988',
+      },
+      {
+        code: 'emp102',
+        name: 'Alex',
+        gender: 'Male',
+        annualSalary: 5700.95,
+        dateOfBirth: '9/6/1982',
+      },
+      {
+        code: 'emp103',
+        name: 'Mike',
+        gender: 'Male',
+        annualSalary: 5900,
+        dateOfBirth: '12/8/1979',
+      },
+      {
+        code: 'emp104',
+        name: 'Mary',
+        gender: 'Female',
+        annualSalary: 6500.826,
+        dateOfBirth: '1/10/1980',
+      },
+      {
+        code: 'emp105',
+        name: 'Nancy',
+        gender: 'Female',
+        annualSalary: 6700.826,
+        dateOfBirth: '1/12/1982',
+      },
+      {
+        code: 'emp106',
+        name: 'Steve',
+        gender: 'Male',
+        annualSalary: 7700.481,
+        dateOfBirth: '11/18/1979',
+      },
+    ];
+  }
 
-    onEmployeeCountChange(selectedvalue : string) : void {
-        this.selectedEmployeeCount =  selectedvalue;
-    }
+  onEmployeeCountChange(selectedvalue: string): void {
+    this.selectedEmployeeCount = selectedvalue;
+  }
 
-    getTotalEmployeesCount(): number {
-        return this.employees.length;
-    }
+  getTotalEmployeesCount(): number {
+    return this.employees.length;
+  }
 
-    getMaleEmployeesCount(): number {
-        return this.employees.filter(e => e.gender === 'Male').length;
-    }
+  getMaleEmployeesCount(): number {
+    return this.employees.filter((e) => e.gender === 'Male').length;
+  }
 
-    getFemaleEmployeesCount(): number {
-        return this.employees.filter(e => e.gender === 'Female').length;
-    }
+  getFemaleEmployeesCount(): number {
+    return this.employees.filter((e) => e.gender === 'Female').length;
+  }
 }
